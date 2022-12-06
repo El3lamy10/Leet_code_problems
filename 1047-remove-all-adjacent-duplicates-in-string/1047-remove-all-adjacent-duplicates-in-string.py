7ccc4532456187c85ecf3ef1f3cc136a219e4fd5
Mohamed Ebrahim <67212677+El3lamy10@users.noreply.github.com>
2022-12-06 23:40:39 +0200

    Delete 1047-remove-all-adjacent-duplicates-in-string directory

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, s: str) -> str:
-        stack=[]
-        for char in s:
-            if  stack and char == stack[-1]:
-                stack.pop()
-            else:
-                stack.append(char)
-        return ''.join(stack)
