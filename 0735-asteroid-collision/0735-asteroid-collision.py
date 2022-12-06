6a0128d2a5011f19925d6765cc34b6048d4638f1
Mohamed Ebrahim <67212677+El3lamy10@users.noreply.github.com>
2022-12-07 00:45:36 +0200

    Time: 102 ms (78.56%), Space: 14.6 MB (46.73%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,26 +1,16 @@
 class Solution(object):
     def asteroidCollision(self, asteroids):
-        res = []
-        for asteroid in asteroids:
-            # We only need to resolve collisions under the following conditions:
-            # - stack is non-empty
-            # - current asteroid is -ve
-            # - top of the stack is +ve
-            while len(res) and asteroid < 0 and res[-1] > 0:
-                # Both asteroids are equal, destroy both.
-                if res[-1] == -asteroid: 
-                    res.pop()
-                    break
-                # Stack top is smaller, remove the +ve asteroid 
-                # from the stack and continue the comparison.
-                elif res[-1] < -asteroid:
-                    res.pop()
+        stack = [] 
+        for ast in asteroids:
+            while stack and ast < 0 < stack[-1]:
+                if stack[-1]< -ast:
+                    stack.pop()
                     continue
-                # Stack top is larger, -ve asteroid is destroyed.
-                elif res[-1] > -asteroid:
+                elif stack[-1]==-ast:
+                    stack.pop()
+                    break
+                elif stack[-1] > -ast:
                     break
             else:
-                # -ve asteroid made it all the way to the 
-                # bottom of the stack and destroyed all asteroids.
-                res.append(asteroid)
-        return res
+                stack.append(ast)
+        return stack
