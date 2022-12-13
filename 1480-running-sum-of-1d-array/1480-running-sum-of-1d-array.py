49681ce95894e943fd647171e4660a3fb72acb48
Mohamed Ebrahim <67212677+El3lamy10@users.noreply.github.com>
2022-12-13 23:53:13 +0200

    Update 1480-running-sum-of-1d-array.py

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -10,3 +10,15 @@ class Solution(object):
             nums[i]+=nums[i-1]
             
         return nums
+    
+    
+##another solve 
+#from itertools import accumulate
+
+#class Solution(object):
+#    def runningSum(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
+#         return list(accumulate(nums))  
